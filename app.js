68a62250cb7996b3bc9542ee5589f032b3afdef1
Zain Mehboob <147308096+ZainMehboob12@users.noreply.github.com>
2023-12-31 23:07:37 +0500

    asignment completed

---
+++ app.js
@@ -0,0 +1,19 @@
+(function() {
+    angular.module('LunchCheck', [])
+      .controller('LunchCheckController', LunchCheckController);
+  
+    LunchCheckController.$inject = ['$scope'];
+  
+    function LunchCheckController($scope) {
+      $scope.message = '';
+  
+      $scope.checkItems = function() {
+        const items = $scope.lunchItems.split(',');
+        $scope.message = items.length <= 3 ? 'Enjoy!' : 'Too much!';
+        if (!items.length) {
+          $scope.message = 'Please enter data first';
+        }
+      };
+    }
+  })();
+  
