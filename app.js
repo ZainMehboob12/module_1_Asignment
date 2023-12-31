8393714687c1dc3e240f2e6cbe8954dd9b0c9b56
Zain Mehboob <147308096+ZainMehboob12@users.noreply.github.com>
2023-12-31 23:50:51 +0500

    updated to add alert messaage

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 (function() {
+    'use strict';
+
     angular.module('LunchCheck', [])
-      .controller('LunchCheckController', LunchCheckController);
-  
+        .controller('LunchCheckController', LunchCheckController);
+
     LunchCheckController.$inject = ['$scope'];
-  
+
     function LunchCheckController($scope) {
-      $scope.message = '';
-  
-      $scope.checkItems = function() {
-        const items = $scope.lunchItems.split(',');
-        $scope.message = items.length <= 3 ? 'Enjoy!' : 'Too much!';
-        if (!items.length) {
-          $scope.message = 'Please enter data first';
-          
-        }
-      };
+        $scope.dishes = '';
+        $scope.message = '';
+        $scope.checked = false;
+
+        $scope.checkLunch = function() {
+            if ($scope.dishes.trim().length === 0) {
+                $scope.empty = true;
+            } else {
+                $scope.checked = true;
+                $scope.empty = false;
+                var arrayDishes = $scope.dishes.split(',');
+                var arrayDishesWithoutEmptys = arrayDishes.filter(function(v) {
+                    return v.trim() !== '';
+                });
+
+                if (arrayDishesWithoutEmptys.length <= 3) {
+                    $scope.message = 'Enjoy!';
+                } else {
+                    $scope.message = 'Too much!';
+                }
+            }
+        };
     }
-  })();
-  
+})();
